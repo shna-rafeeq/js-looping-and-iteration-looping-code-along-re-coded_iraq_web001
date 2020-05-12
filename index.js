a7531e262613c4abdfe1e196d7f7c9285bf074f9
shna rafeeq <shnar.majeed@gmail.com>
2020-05-12 16:54:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+const girls =["Ada", "Brendan", "Ali"];
+const eventName = 'surprice';
+ 
+function writeCards(girls,eventName) {
+  const result = [];
+  for (let i = 0; i < girls.length; i++) {
+    result.push(`Thank you, ${girls[i]}, for the wonderful ${eventName} gift!`);
+  }
+ 
+  return result;
+}
+ writeCards(girls,eventName);
+ 
+ 
+ function countDown(num) {
+    let i = num;
+    while(i>-1){
+        console.log(i);
+        i--;
+      
+  }
+ 
+  }
+ countDown(10);
